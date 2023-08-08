1fb243321ee5139dbb7527fe2f553a8e46525c9a
Siddique Data Engineer <dba.siddique@gmail.com>
2023-08-08 14:33:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+102 Aws Data Lake Platform - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 102-aws-data-lake-platform")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
